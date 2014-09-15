078afc0a2e2ffa23b7044ab2dd31c479ab595dfa
daichi <prinum101@gmail.com>
2014-09-16 03:25:28 +0900

    PointArea追加

--- js/app.js
+++ js/app.js
@@ -15,6 +15,10 @@ window.onload = function() {
         w: 25,
         h: 50
       }
+    },
+    PointArea: {
+      w: 120,
+      h: 50
     }
   };
 
@@ -26,7 +30,9 @@ window.onload = function() {
     game.rootScene.backgroundColor  = '#7ecef4'; // ゲームの動作部分の背景色を設定しています。
     var airplane = new Airplane();
     // var piron = new Piron();
-    var pirons = new Pirons();
+    setInterval(function(){
+      var pirons = new Pirons();
+    }, 2000);
   }
 
   game.start();
@@ -34,28 +40,28 @@ window.onload = function() {
   Airplane = enchant.Class.create(Sprite, {
     initialize: function() {
       var spriteName = 'airplane';
-      var self = this;
       var game = enchant.Game.instance;
       var width = Config.Sprite[spriteName].w;
       var height = Config.Sprite[spriteName].h;
-      Sprite.call(self, width, height);
-      self.image = game.assets['../images/' + spriteName + '.png']; // あらかじめロードしておいた画像を適用します。
-      self.x = (Config.Window.w / 2) - (width / 2);
-      self.y = Config.Window.h - height;
-      game.rootScene.addChild(self); // ゲームのシーンにくまを表示させます。
+      Sprite.call(this, width, height);
+      this.image = game.assets['../images/' + spriteName + '.png']; // あらかじめロードしておいた画像を適用します。
+      this.x = (Config.Window.w / 2) - (width / 2);
+      this.y = Config.Window.h - height;
+      game.rootScene.addChild(this); // ゲームのシーンにくまを表示させます。
       
-      var speed = 0;
-      self.addEventListener(Event.ENTER_FRAME, function() {
-        self.x += speed;
-      });
+      this.speed = 0;
 
+      var self = this;
       game.rootScene.addEventListener(Event.TOUCH_START, function(e) {
         if (e.x > self.x) {
-          speed += 1;
+          self.speed += 1;
         } else {
-          speed -= 1;
+          self.speed -= 1;
         }
       });
+    },
+    onenterframe: function() {
+        this.x += this.speed;
     }
   });
 
@@ -67,10 +73,21 @@ window.onload = function() {
       var height = Config.Sprite[spriteName].h;
       var game = enchant.Game.instance;
       Sprite.call(self, width, height);
+      
       self.image = game.assets['../images/' + spriteName + '.png'];
       self.x = Config.Window.w / 2 - (width / 2);
-
       game.rootScene.addChild(self);
+    }
+  });
+
+  PointArea = enchant.Class.create(Sprite, {
+    initialize: function() {
+      Sprite.call(this, Config.PointArea.w, Config.PointArea.h);
+      this.backgroundColor = '#000';
+      this.x = (Config.Window.w / 2) - (Config.PointArea.w / 2);
+      this.opacity = 0.5;
+      var game = enchant.Game.instance;
+      game.rootScene.addChild(this);
     }
   });
 
@@ -78,26 +95,40 @@ window.onload = function() {
     initialize: function() {
       Group.call(this);
       var leftPiron = new Piron();
-      leftPiron.x -= 60;
+      leftPiron.x -= Config.PointArea.w / 2;
       this.addChild(leftPiron);
-      var rightPiron = new Piron();
-      rightPiron.x += 60;
-      this.addChild(rightPiron);
 
-      var initialY = 0;
-      this.y = initialY;
-      var speed = 5;
+      var rightPiron = new Piron();
+      rightPiron.x += Config.PointArea.w / 2;
+      this.addChild(rightPiron);
+      
+      var pointArea = new PointArea();
+      this.addChild(pointArea);
+
+      function randomX() {
+        return Math.random() * (Config.Window.w / 2) * randomPlusOrMinus();// TODO 調整
+      }
+      this.x = randomX();
+      this.y = - Config.Sprite.piron.h;
 
       var game = enchant.Game.instance;
       game.rootScene.addChild(this);
-
-      var self = this;
-      game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
-        self.y += speed;
-        if (self.y > Config.Window.h) {
-          self.y = initialY;
-        }
-      });
+    },
+    onenterframe: function() {
+      var speed = 5;
+      this.y += speed;
+      if (this.y > Config.Window.h) {
+        var game = enchant.Game.instance;
+        game.rootScene.removeChild(this);
+      }
     }
   });
+
+  function randomPlusOrMinus(){
+    if (Math.random() > 0.5) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
 };
